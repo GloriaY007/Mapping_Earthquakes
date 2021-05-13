070f767bc897c1d7d504d8bbc865288f5f9dd0d4
GloriaY007 <glorrossy@gmail.com>
2021-05-13 16:36:40 -0400

    Added Mapping_Lines folder

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,72 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//  Add a marker to the map for Los Angeles, California.
+//Code = let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle marker to the map for Los Angeles, California
+//L.circle([34.0522, -118.2437], {
+//    color: 'black',
+//    fillColor: '#FFFF99',
+//    fillOpacity: 0.5,
+//    radius: 300
+// }).addTo(map);
+
+// Add a yellow circle marker to the map for Los Angeles, California using circleMarker() function
+// L.circleMarker([34.0522, -118.2437], {
+//    color: "black",
+//    fillColor: '#FFFFa1',
+//    radius: 300
+//}).addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: '#FFA500',
+        fillOpacity: 0.3,
+        weight: 4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// To change the map's style, change the map id using the list of Mapbox ids below:
+// mapbox.streets
+// mapbox.light
+// mapbox.dark
+// mapbox.satellite
+// mapbox.streets-satellite
+// mapbox.wheatpaste
+// mapbox.streets-basic
+// mapbox.comic
+// mapbox.outdoors
+// mapbox.run-bike-hike
+// mapbox.pirates
+// mapbox.emerald
+// mapbox.high-contrast
+
+// Use python -m http.server to show index file
