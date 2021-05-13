a7c552cac9fc993b71032fa70e88d11584764174
GloriaY007 <glorrossy@gmail.com>
2021-05-13 16:59:24 -0400

    Mapped lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,50 +2,27 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([37.6213, -73.7781], 5);
 
-//  Add a marker to the map for Los Angeles, California.
-//Code = let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Create a line from LAX to SFO - line from LAX to SFO
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [38.9531, -77.4565],
+    [40.6413, -73.7781]
+  ];
 
-// Add a circle marker to the map for Los Angeles, California
-//L.circle([34.0522, -118.2437], {
-//    color: 'black',
-//    fillColor: '#FFFF99',
-//    fillOpacity: 0.5,
-//    radius: 300
-// }).addTo(map);
-
-// Add a yellow circle marker to the map for Los Angeles, California using circleMarker() function
-// L.circleMarker([34.0522, -118.2437], {
-//    color: "black",
-//    fillColor: '#FFFFa1',
-//    radius: 300
-//}).addTo(map);
-
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location).addTo(map);
-// });
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        color: '#FFA500',
-        fillOpacity: 0.3,
-        weight: 4
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    weight: 4,
+    fillOpacity: 0.5,
+    dashArray: '5,10'
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
